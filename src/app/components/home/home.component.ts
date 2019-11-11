d3a336f1f8d715921e48ddb1fc84e415601f3589
tgrimault21 <t.grimault21@gmail.com>
2019-11-11 23:52:11 +0100

    10 movies list with informations

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { InformationsComponent } from '../informations/informations.component';
 
 
 @Component({
@@ -10,20 +12,52 @@ import { HttpClient } from '@angular/common/http'
 
 export class HomeComponent implements OnInit {
 
-  data: any;
+  movies: any;
+  dataMovie: any[] = [];
+  nbDisplay: number = 10;
+  extract: any;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, public dialog: MatDialog) {
   }
 
   ngOnInit() {
     this.getMovies();
   }
 
+  console(data) {
+    return console.log(data);
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(InformationsComponent, {
+      width: '250px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+
   getMovies() {
     this.http.get("https://api.themoviedb.org/3/movie/popular?page=1&language=en-US&api_key=9e2b8a1d23b0a9148f8bb5bf8f512bd8").subscribe(res => {
-      this.data = res;
-      console.log(this.data)
+      this.movies = res;
+      console.log({message: "movies", data: this.movies})
+      this.extraction(this.movies, this.nbDisplay);
     });
+  }
+
+  extraction(movies, nbDisplay) {
+    this.extract = movies.results.slice(0,nbDisplay);
+    this.getMovieDetails(movies);
+  }
+
+  getMovieDetails(movies) {
+    let me = this;
+    movies.results.forEach(function(movie) {
+      me.http.get("https://api.themoviedb.org/3/movie/" + movie.id + "?language=en-US&api_key=9e2b8a1d23b0a9148f8bb5bf8f512bd8").subscribe(res => {
+        me.dataMovie.push(res);
+      });
+    })
   }
 
 }
---
+++ src/app/components/informations/informations.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
+
+@Component({
+  selector: 'app-informations',
+  templateUrl: './informations.component.html',
+  styleUrls: ['./informations.component.css']
+})
+export class InformationsComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<InformationsComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  ngOnInit() {
+  }
+
+}
