5a38a69eb6c5dc74baec98bce159dcc24a48da67
tgrimault21 <t.grimault21@gmail.com>
2019-11-12 23:52:29 +0100

    search + genres filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import { InformationsComponent } from './components/informations/informations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InformationsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatDialogModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [InformationsComponent]
 })
 export class AppModule { }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { InformationsComponent } from '../informations/informations.component';
+import {NgForm} from '@angular/forms';
 
+export interface Genres {
+  genresAPI: any[];
+}
 
 @Component({
   selector: 'app-home',
@@ -12,20 +16,71 @@ import { InformationsComponent } from '../informations/informations.component';
 
 export class HomeComponent implements OnInit {
 
-  movies: any;
+  popularMovies: any;
   dataMovie: any[] = [];
   nbDisplay: number = 10;
   extract: any;
+  genresList: string[] = [];
+  genresInterface: Genres;
+  genreIDs: any[] = [];
+  moviesAfterGenreFilter: any[] = [];
 
   constructor(private http: HttpClient, public dialog: MatDialog) {
   }
 
   ngOnInit() {
-    this.getMovies();
+    if(this.dataMovie.length == 0) {
+      this.getPopularMovies();
+    }
+    this.getGenres();
+  }
+
+  getGenres() {
+    this.http.get<Genres>("https://api.themoviedb.org/3/genre/movie/list?api_key=9e2b8a1d23b0a9148f8bb5bf8f512bd8&language=en-US").subscribe(res => {
+      this.genresInterface = {
+        genresAPI: (res as any).genres
+      }
+      this.genresInterface.genresAPI.map(genre => {
+        this.genresList.push(genre);
+      })
+    });
+  }
+
+  genreFilter(values) {
+    console.log({message: "data movies", data: this.dataMovie})
   }
 
   console(data) {
     return console.log(data);
+  }
+
+  movieResearch(search: NgForm) {
+    var i = 0;
+    console.log(search);
+    if(search.form.value.searchMovie){
+      this.http.get("https://api.themoviedb.org/3/search/movie?api_key=9e2b8a1d23b0a9148f8bb5bf8f512bd8&language=en-US&include_adult=false&query=" + search.form.value.searchMovie).subscribe(res => {
+        this.dataMovie = [];
+        this.genreIDs = [];
+        search.form.value.filterGenres.map(genre => {
+          this.genreIDs.push(genre.id);
+        });
+        this.moviesAfterGenreFilter = (res as any).results.filter(movie => {
+          movie.genre_ids.map(id => {
+            i += this.genreIDs.indexOf(id);
+          })
+          if(i > 0) {
+            return true;
+          } else {
+            return false;
+          }
+        })
+        console.log({message: "ids", data: this.genreIDs})
+        console.log({message: "movies", data: this.moviesAfterGenreFilter})
+        this.extraction(this.moviesAfterGenreFilter, this.nbDisplay);
+      });
+    } else {
+      this.getPopularMovies();
+    }
   }
 
   openDialog(): void {
@@ -38,22 +93,27 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  getMovies() {
-    this.http.get("https://api.themoviedb.org/3/movie/popular?page=1&language=en-US&api_key=9e2b8a1d23b0a9148f8bb5bf8f512bd8").subscribe(res => {
-      this.movies = res;
-      console.log({message: "movies", data: this.movies})
-      this.extraction(this.movies, this.nbDisplay);
+  getPopularMovies() {
+    this.http.get("https://api.themoviedb.org/3/movie/popular?language=en-US&api_key=9e2b8a1d23b0a9148f8bb5bf8f512bd8").subscribe(res => {
+      this.popularMovies = res;
+      console.log({message: "popularMovies", data: this.popularMovies})
+      this.extraction(this.popularMovies, this.nbDisplay);
     });
   }
 
   extraction(movies, nbDisplay) {
-    this.extract = movies.results.slice(0,nbDisplay);
+    if(movies.results) {
+      this.extract = movies.results.slice(0,nbDisplay);
+      movies = movies.results;
+    } else {
+      this.extract = movies.slice(0,nbDisplay);
+    }
     this.getMovieDetails(movies);
   }
 
   getMovieDetails(movies) {
     let me = this;
-    movies.results.forEach(function(movie) {
+    movies.forEach(function(movie) {
       me.http.get("https://api.themoviedb.org/3/movie/" + movie.id + "?language=en-US&api_key=9e2b8a1d23b0a9148f8bb5bf8f512bd8").subscribe(res => {
         me.dataMovie.push(res);
       });
