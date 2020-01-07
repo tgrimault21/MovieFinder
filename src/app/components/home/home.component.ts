1afd68014ff544f109cc29a035e6820e88548ee8
Vincent Tabary <vincent.tabary@gmail.com>
2020-01-07 15:38:52 +0100

    fix: convention examples

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { InformationsComponent } from '../informations/informations.component';
-import { TowatchlistComponent } from '../towatchlist/towatchlist.component';
-import { WatchedlistComponent } from '../watchedlist/watchedlist.component';
-import {NgForm} from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
 export interface Genres {
   genresAPI: any[];
@@ -15,51 +13,59 @@ export interface Genres {
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-
 export class HomeComponent implements OnInit {
+  public genresList: string[] = [];
+  public dataMovie: any[] = [];
+  public filterReleasedAfter = '2000';
+  public filterReleasedBefore = '2020';
 
-  popularMovies: any;
-  dataMovie: any[] = [];
-  nbDisplay: number = 10;
-  extract: any;
-  genresList: string[] = [];
-  genresInterface: Genres;
-  genreIDs: any[] = [];
-  moviesAfterGenreFilter: any = [];
-  filter2 = "2000";
-  filter3 = "2020";
-  storageWatched: number[] = [];
-  storageToWatch: any[] = [];
+  public nbDisplay = 10;
+  public extract: any;
+  public genresInterface: Genres;
+  public genreIDs: any[] = [];
+  public moviesAfterGenreFilter: any = [];
+  public storageWatched: number[] = [];
+  public storageToWatch: number[] = [];
 
-  constructor(private http: HttpClient, public dialog: MatDialog) {
-  }
+  constructor(
+    private http: HttpClient,
+    private dialog: MatDialog
+  ) {}
 
-  ngOnInit() {
-    if(this.dataMovie.length == 0) {
+  public ngOnInit() {
+    if(this.dataMovie.length === 0) {
       this.getPopularMovies(null);
     }
+
     this.getGenres();
   }
 
-  //get the list of each genre existing in API database and display them in the filter
-  getGenres() {
-    this.http.get<Genres>("https://api.themoviedb.org/3/genre/movie/list?api_key=9e2b8a1d23b0a9148f8bb5bf8f512bd8&language=en-US").subscribe(res => {
-      this.genresInterface = {
-        genresAPI: (res as any).genres
-      }
-      this.genresInterface.genresAPI.map(genre => {
-        this.genresList.push(genre);
-      })
-    });
+  /**
+   * Get the list of each genre existing in API database and display them in the filter
+   */
+  public getGenres() {
+    this.http.get<{ genres: Genres }>('https://api.themoviedb.org/3/genre/movie/list?api_key=9e2b8a1d23b0a9148f8bb5bf8f512bd8&language=en-US')
+      .subscribe(res => {
+        this.genresInterface = {
+          genresAPI: res.genres
+        };
+
+        this.genresInterface.genresAPI.map(genre => {
+          this.genresList.push(genre);
+        });
+      });
   }
 
-  //To use when we want to test data in html
-  console(data) {
-    return console.log(data);
+  /**
+   * To use when we want to test data in html
+   * @param data something to display
+   */
+  public console(data: any) {
+    console.log(data);
   }
 
   //Add or remove a movie of a list when we click on a toggle button
-  addWatched(id, watchedlist) {
+  public addWatched(id, watchedlist) {
     this.setStorageWatched();
     if(!this.storageWatched.includes(id)){
       this.storageWatched.push(id);
@@ -67,11 +73,12 @@ export class HomeComponent implements OnInit {
       let index = this.storageWatched.indexOf(id);
       this.storageWatched.splice(index,1);
     }
-    localStorage.setItem("watched", JSON.stringify(this.storageWatched));
-    //put the movie in the watched list to display on "Watched movies" tab
+    localStorage.setItem('watched', JSON.stringify(this.storageWatched));
+    //put the movie in the watched list to display on 'Watched movies' tab
     watchedlist.getMoviesWatched();
   }
-  addToWatch(id, towatchlist) {
+
+  public addToWatch(id, towatchlist) {
     this.setStorageToWatch();
     if(!this.storageToWatch.includes(id)){
       this.storageToWatch.push(id);
@@ -79,36 +86,42 @@ export class HomeComponent implements OnInit {
       let index = this.storageToWatch.indexOf(id);
       this.storageToWatch.splice(index,1);
     }
-    localStorage.setItem("toWatch", JSON.stringify(this.storageToWatch));
-    //put the movie in the watched list to display on "Movies To Watch" tab
+    localStorage.setItem('toWatch', JSON.stringify(this.storageToWatch));
+    //put the movie in the watched list to display on 'Movies To Watch' tab
     towatchlist.getMoviesToWatch();
   }
 
-  //To keep each toggle button pressed if the movie we test is in the watched or to watch list
-  isWatched(id) {
-    return(JSON.parse(localStorage.getItem("watched")) && JSON.parse(localStorage.getItem("watched")).includes(id));
+  /**
+   * To keep each toggle button pressed if the movie we test is in the watched or to watch list
+   * @param id the identifier of a media
+   * @returns true if the media is watched by the user
+   */
+  public isWatched(id: number): boolean {
+    return JSON.parse(localStorage.getItem('watched')) && JSON.parse(localStorage.getItem('watched')).includes(id);
   }
-  isToWatch(id) {
-    return(JSON.parse(localStorage.getItem("toWatch")) && JSON.parse(localStorage.getItem("toWatch")).includes(id));
+
+  public isToWatch(id) {
+    return(JSON.parse(localStorage.getItem('toWatch')) && JSON.parse(localStorage.getItem('toWatch')).includes(id));
   }
 
   //Put local storage content in an array
-  setStorageWatched() {
-    if(JSON.parse(localStorage.getItem("watched"))) this.storageWatched = JSON.parse(localStorage.getItem("watched"));
+  public setStorageWatched() {
+    if(JSON.parse(localStorage.getItem('watched'))) this.storageWatched = JSON.parse(localStorage.getItem('watched'));
   }
-  setStorageToWatch() {
-    if(JSON.parse(localStorage.getItem("toWatch"))) this.storageToWatch = JSON.parse(localStorage.getItem("toWatch"));
+
+  public setStorageToWatch() {
+    if(JSON.parse(localStorage.getItem('toWatch'))) this.storageToWatch = JSON.parse(localStorage.getItem('toWatch'));
   }
 
   //Split year from complete date
-  getYear(releaseDate) {
-    releaseDate = releaseDate.split("-");
+  public getYear(releaseDate) {
+    releaseDate = releaseDate.split('-');
     var year = releaseDate[0];
     return year;
   }
 
   //Test if one of the genre selected in the filter is associated to each movie
-  filterMoviesByGenre(filterGenres, res) {
+  public filterMoviesByGenre(filterGenres, res) {
     var i = 0;
     this.genreIDs = [];
     //push the ids of each genre selected in an array
@@ -125,21 +138,22 @@ export class HomeComponent implements OnInit {
   }
 
   //Comparing the year of the release date of the film with the year in filter input
-  filterMoviesByReleaseDateAfter(filterYear, res) {
+  public filterMoviesByReleaseDateAfter(filterYear, res) {
     this.moviesAfterGenreFilter = res.filter(movie => this.getYear(movie.release_date) >= filterYear.toString())
   }
-  filterMoviesByReleaseDateBefore(filterYear, res) {
+
+  public filterMoviesByReleaseDateBefore(filterYear, res) {
     this.moviesAfterGenreFilter = res.filter(movie => this.getYear(movie.release_date) < filterYear.toString())
   }
 
   //Called when the form is submited
-  movieResearch(search: NgForm) {
+  public movieResearch(search: NgForm) {
     //clear release date filters field when they aren't valid input
-    if(!search.form.value.filterYearAfter || search.form.value.filterYearAfter>"2019") this.filter2 = null;
-    if(!search.form.value.filterYearBefore || search.form.value.filterYearBefore>"2020" || search.form.value.filterYearBefore<=search.form.value.filterYearAfter) this.filter3 = null;
-    //This will search through the database based on the "name of the movie" input, and then we apply filters on the result
+    if(!search.form.value.filterYearAfter || search.form.value.filterYearAfter>'2019') this.filterReleasedAfter = null;
+    if(!search.form.value.filterYearBefore || search.form.value.filterYearBefore>'2020' || search.form.value.filterYearBefore<=search.form.value.filterYearAfter) this.filterReleasedBefore = null;
+    //This will search through the database based on the 'name of the movie' input, and then we apply filters on the result
     if(search.form.value.searchMovie){
-      this.http.get("https://api.themoviedb.org/3/search/movie?api_key=9e2b8a1d23b0a9148f8bb5bf8f512bd8&language=en-US&include_adult=false&query=" + search.form.value.searchMovie).subscribe(res => {
+      this.http.get('https://api.themoviedb.org/3/search/movie?api_key=9e2b8a1d23b0a9148f8bb5bf8f512bd8&language=en-US&include_adult=false&query=' + search.form.value.searchMovie).subscribe(res => {
         this.dataMovie = [];
         if(search.form.value.filterGenres && search.form.value.filterGenres.length!=0) {
           console.log(search.form.value.filterGenres);
@@ -159,16 +173,16 @@ export class HomeComponent implements OnInit {
   }
 
   //Show the list of top popular movies by default
-  getPopularMovies(search: NgForm) {
-    this.http.get("https://api.themoviedb.org/3/movie/popular?language=en-US&api_key=9e2b8a1d23b0a9148f8bb5bf8f512bd8").subscribe(res => {
+  public getPopularMovies(search: NgForm) {
+    this.http.get('https://api.themoviedb.org/3/movie/popular?language=en-US&api_key=9e2b8a1d23b0a9148f8bb5bf8f512bd8').subscribe(res => {
       this.extraction(res, this.nbDisplay);
     });
   }
 
   //Only 10 movies will be displayed on the screen
-  extraction(movies, nbDisplay) {
+  public extraction(movies, nbDisplay) {
     if(movies.results) {
-      //depending on where extraction() is called, movies will sometimes already be "movies.results"
+      //depending on where extraction() is called, movies will sometimes already be 'movies.results'
       this.extract = movies.results.slice(0,nbDisplay);
       movies = movies.results;
     } else {
@@ -178,17 +192,17 @@ export class HomeComponent implements OnInit {
   }
 
   //Put details of every movie shown on the screen in an array
-  getMovieDetails(movies) {
-    let me = this; //to avoid "this is undefined" error
+  public getMovieDetails(movies) {
+    let me = this; //to avoid 'this is undefined' error
     movies.forEach(function(movie) {
-      me.http.get("https://api.themoviedb.org/3/movie/" + movie.id + "?language=en-US&api_key=9e2b8a1d23b0a9148f8bb5bf8f512bd8").subscribe(res => {
+      me.http.get('https://api.themoviedb.org/3/movie/' + movie.id + '?language=en-US&api_key=9e2b8a1d23b0a9148f8bb5bf8f512bd8').subscribe(res => {
         me.dataMovie.push(res);
       });
     })
   }
 
-  //Launch InformationsComponent on a modal when we click on "More details" button
-  openDialog(result): void {
+  //Launch InformationsComponent on a modal when we click on 'More details' button
+  public openDialog(result): void {
     const dialogRef = this.dialog.open(InformationsComponent, {
       width: '450px',
       data: result
